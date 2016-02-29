9bb7b1b6fecedf3d033fb3d795c08f39c8d8eae8
berkland@us.ibm.com <berkland@us.ibm.com>
2016-02-29 10:09:23 -0600

    allow single test to be run

--- src/test/java/org/eclairjs/nashorn/JSRunner.java
+++ src/test/java/org/eclairjs/nashorn/JSRunner.java
@@ -6,6 +6,7 @@ import static java.util.Arrays.asList;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.script.ScriptEngine;
@@ -21,17 +22,36 @@ import org.junit.runner.manipulation.Sortable;
 import org.junit.runner.manipulation.Sorter;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.ParentRunner;
+import org.junit.runners.model.InitializationError;
 
 public class JSRunner extends Runner implements Filterable, Sortable  {
 
     private List<TestClass> tests;
+    private List<TestCaseEntry> filteredTests;
     private final Class<?> cls;
 
-    public JSRunner(Class<?> cls) {
+
+    static class TestCaseEntry {
+        Description description;
+        TestClass testClass;
+        TestCase testCase;
+        TestCaseEntry(Description description, TestClass testClass, TestCase testCase)
+        {
+            this.description=description;
+            this.testClass=testClass;
+            this.testCase=testCase;
+        }
+    }
+
+    private List<TestCaseEntry> testCases=new ArrayList<>();
+
+    public JSRunner(Class<?> cls)  throws InitializationError {
         this.cls = cls;
         List<String> testNames = asList(cls.getAnnotation(Tests.class).value());
         this.tests = findJSTests(testNames);
     }
+
 
     @Override
     public Description getDescription() {
@@ -43,6 +63,7 @@ public class JSRunner extends Runner implements Filterable, Sortable  {
             for (TestCase test : tests) {
                 Description methodDesc = Description.createTestDescription(testClass.junitName(), test.name);
                 desc.addChild(methodDesc);
+                testCases.add(new TestCaseEntry(Description.createTestDescription(cls.getName(), test.name),testClass, test));
             }
         }
         return suite;
@@ -50,18 +71,31 @@ public class JSRunner extends Runner implements Filterable, Sortable  {
 
     @Override
     public void run(RunNotifier notifier) {
-        for (TestClass testClass : tests) {
-            List<TestCase> tests = testClass.testCases;
-            for (TestCase test : tests) {
-                Description desc = Description.createTestDescription(testClass.junitName(), test.name);
-                notifier.fireTestStarted(desc);
-                try {
-                    test.testCase.run();
-                    notifier.fireTestFinished(desc);
-                } catch (Exception | Error e) {
-                    notifier.fireTestFailure(new Failure(desc, e.getCause() != null ? e.getCause() : e));
+        if (filteredTests==null || filteredTests.isEmpty())
+        {
+            for (TestClass testClass : tests) {
+                List<TestCase> tests = testClass.testCases;
+                for (TestCase test : tests) {
+                    runTestCase(notifier, testClass, test);
                 }
             }
+        }
+        else {
+            for (TestCaseEntry test : filteredTests) {
+                runTestCase(notifier, test.testClass, test.testCase);
+            }
+
+        }
+    }
+
+    private void runTestCase(RunNotifier notifier, TestClass testClass, TestCase test) {
+        Description desc = Description.createTestDescription(testClass.junitName(), test.name);
+        notifier.fireTestStarted(desc);
+        try {
+            test.testCase.run();
+            notifier.fireTestFinished(desc);
+        } catch (Exception | Error e) {
+            notifier.fireTestFailure(new Failure(desc, e.getCause() != null ? e.getCause() : e));
         }
     }
 
@@ -95,11 +129,24 @@ public class JSRunner extends Runner implements Filterable, Sortable  {
     }
 
     public void sort(Sorter sorter) {
+        System.out.println("SORT = ==================");
         //
     }
 
     public void filter(Filter filter) throws NoTestsRemainException {
         //
+        if (testCases.isEmpty())
+            getDescription();
+        for (TestCaseEntry entry :testCases)
+        {
+            if (filter.shouldRun(entry.description)) {
+                if (filteredTests== null)
+                    filteredTests= new ArrayList<TestCaseEntry>();
+                filteredTests.add(entry);
+            }
+
+        }
+
     }
 
 
