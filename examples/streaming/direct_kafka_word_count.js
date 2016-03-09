a68f06e4bf7139995688c7973305bb05929aec15
berkland@us.ibm.com <berkland@us.ibm.com>
2016-03-09 17:25:18 -0600

    add direct kafka streaming example and it's supporting APIs

---
+++ examples/streaming/direct_kafka_word_count.js
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2016 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Consumes messages from one or more topics in Kafka and does wordcount.
+ * Usage: bin/eclairjs.sh examples/streaming/direct_kafka_word_count.js <brokers> <topics>
+ *   <brokers> is a list of one or more Kafka brokers
+ *   <topics> is a list of one or more kafka topics to consume from
+ *
+ * Example:
+ *    $ bin/eclairjs.sh examples/streaming/direct_kafka_word_count.js  broker1-host:port,broker2-host:port topic1,topic2
+ */
+
+if ((typeof args == "undefined")||args.length<3)
+{
+          print(
+          "Usage: bin/eclairjs.sh examples/streaming/direct_kafka_word_count.js  <brokers> <topics>\n" +
+          "  <brokers> is a list of one or more Kafka brokers\n" +
+          "  <topics> is a list of one or more kafka topics to consume from\n\n");
+          java.lang.System.exit(-1);
+}
+
+    var brokers = args[1];
+    var topics = args[2];
+    // Create the context with a 1 second batch size
+
+    var conf = new SparkConf().setAppName("Javascript Direct Kafka Word Count").setMaster("local[*]");
+    var jssc = new StreamingContext(conf, new Duration(2000));
+
+
+
+    var topicsList = topics.split(",");
+    var kafkaParams = {
+        "metadata.broker.list": brokers
+    };
+
+    // Create direct kafka stream with brokers and topics
+    var messages = KafkaUtils.createDirectStream(
+        jssc,
+        kafkaParams,
+        topicsList
+    );
+
+    // Get the lines, split them into words, count the words and print
+    var lines = messages.map(function(tuple2) {
+        return tuple2[1];
+    });
+    var words = lines.flatMap( function( x) {
+        return Lists.newArrayList(SPACE.split(x));
+    });
+    var wordCounts = words.mapToPair(function( s) {
+          return new Tuple(s, 1);
+      }).reduceByKey(
+        function( i1,  i2) {
+          return i1 + i2;
+      });
+    wordCounts.print();
+
+    // Start the computation
+    jssc.start();
+    jssc.awaitTermination();
+
--- src/main/resources/streaming/KafkaUtils.js
+++ src/main/resources/streaming/KafkaUtils.js
@@ -15,8 +15,13 @@
  */
 
 //var KafkaUtils = function() {}
-var JavaKakfaUtils = Java.type("org.apache.spark.streaming.kafka.KafkaUtils")
-var KafkaUtils = {}
+
+var JavaKakfaUtils = Java.type("org.apache.spark.streaming.kafka.KafkaUtils");
+var KafkaUtils = {};
+
+var Class = Java.type("java.lang.Class");
+var StringClass = Class.forName("java.lang.String");
+var StringDecoderClass=Class.forName("kafka.serializer.StringDecoder");
 
 KafkaUtils.createStream = function(ssc, zkQuorum, group, topic) {
     var integer = new java.lang.Integer(1);
@@ -27,4 +32,51 @@ KafkaUtils.createStream = function(ssc, zkQuorum, group, topic) {
                                                    group,
                                                    m),
                        ssc);
+};
+
+
+/**
+ *  NOTE: This currently only works on Kafka key/values which are Strings
+ *
+ * Create an input stream that directly pulls messages from Kafka Brokers
+ * without using any receiver. This stream can guarantee that each message
+ * from Kafka is included in transformations exactly once (see points below).
+ *
+ * Points to note:
+ *  - No receivers: This stream does not use any receiver. It directly queries Kafka
+ *  - Offsets: This does not use Zookeeper to store offsets. The consumed offsets are tracked
+ *    by the stream itself. For interoperability with Kafka monitoring tools that depend on
+ *    Zookeeper, you have to update Kafka/Zookeeper yourself from the streaming application.
+ *    You can access the offsets used in each batch from the generated RDDs (see
+ *    {@link HasOffsetRanges}).
+ *  - Failure Recovery: To recover from driver failures, you have to enable checkpointing
+ *    in the {@link StreamingContext}. The information on consumed offset can be
+ *    recovered from the checkpoint. See the programming guide for details (constraints, etc.).
+ *  - End-to-end semantics: This stream ensures that every records is effectively received and
+ *    transformed exactly once, but gives no guarantees on whether the transformed data are
+ *    outputted exactly once. For end-to-end exactly-once semantics, you have to either ensure
+ *    that the output operation is idempotent, or use transactions to output records atomically.
+ *    See the programming guide for more details.
+ *
+ * @param {StreamingContext} ssc  StreamingContext object
+ * @param {object} kafkaParams  map of Kafka options (key, value). Kafka <a href="http://kafka.apache.org/documentation.html#configuration">
+ *   configuration parameters</a>. Requires "metadata.broker.list" or "bootstrap.servers"
+ *   to be set with Kafka broker(s) (NOT zookeeper servers), specified in
+ *   host1:port1,host2:port2 form.
+ *   If not starting from a checkpoint, "auto.offset.reset" may be set to "largest" or "smallest"
+ *   to determine where the stream starts (defaults to "largest")
+ * @param {string[]} topics  Names of the topics to consume
+ * @returns {DStream}  DStream of (Kafka message key, Kafka message value)
+ */
+KafkaUtils.createDirectStream = function(ssc,kafkaParams,topics) {
+  var ssc_uw = Utils.unwrapObject(ssc);
+  var kafkaParams_uw = Utils.createJavaHashMap(kafkaParams);
+  var topics_uw = Utils.createJavaSet(topics);
+  var javaObject =  JavaKakfaUtils.createDirectStream(ssc_uw,
+    StringClass,
+        StringClass,
+        StringDecoderClass,
+        StringDecoderClass,
+        kafkaParams_uw,topics_uw);
+  return new DStream(javaObject,ssc);
 };
